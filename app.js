855a8ac052f0e36d37077c43dfabe176f725c190
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-10-01 16:24:38 -0700

    Added userFirstName variable and made my alerts more personal

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 'use strict';
 
-alert('Hello and Welcome! Let\'s play a game! My name is Stephen and I have a few yes or no questions for you... about me! Take you\'re best guess!');
+alert('Hello and Welcome! Let\'s play a game! My name is Stephen and I have a few yes or no questions for you... about me!');
+alert('Actually, one question about you...');
+
+var userFirstName = prompt('What is your first name?');
+
+alert('Hi ' + userFirstName + '! Hope you enjoy the game I made for you! 5 simple yes or no questions - Take you\'re best guess!');
 
 var lovesCoffee = prompt('Do I like coffee?');
 lovesCoffee = lovesCoffee.toLowerCase();
 
 if(lovesCoffee.includes('yes')) {
   console.log(lovesCoffee);
-  //console.log('I love coffee! CoffeeCoffeeCoffee!');
-  alert('Correct!');
+  console.log('Correct');
+  alert('Correct! Nice work ' + userFirstName + '!');
   alert('I love coffee! CoffeeCoffeeCoffee!');
 } else if(lovesCoffee.includes('no')){
   console.log(lovesCoffee);
-  //console.log('I could not survive without it!');
-  alert('Incorrect.');
+  console.log('Incorrect.');
+  alert('Sorry. That is not correct ' + userFirstName + '.');
   alert('I don\'t like coffee... I LOVE it! I don\'t think I could survive without it!');
 } else {
   console.log(lovesCoffee);
@@ -26,18 +31,19 @@ newZealand = newZealand.toLowerCase();
 
 if(newZealand.includes('yes')) {
   console.log(newZealand);
+  console.log('Incorrect.');
   //console.log('Incorrect. I would love to visit but I was actually born in Montana.');
-  alert('Incorrect.');
+  alert('Sorry. That is not correct ' + userFirstName + '.');
   alert('I would love to visit but I was actually born in Montana.');
 } else if(newZealand.includes('no') && lovesCoffee.includes('yes')) {
   console.log(newZealand);
-  //console.log('Correct again! It is a dream of mine to visit New Zealand someday. I was actually born in Montana.');
-  alert('Correct again!!!');
+  console.log('Correct');
+  alert('Correct again ' + userFirstName + '!!!');
   alert('It is a dream of mine to visit New Zealand someday. I was actually born in Montana.');
 } else if(newZealand.includes('no')) {
   console.log(newZealand);
-  //console.log('Correct! It is a dream of mine to visit New Zealand someday. I was actually born in Montana.');
-  alert('Correct!');
+  console.log('Correct');
+  alert('Correct! Nice work ' + userFirstName + '!');
   alert('It is a dream of mine to visit New Zealand someday. I was actually born in Montana.');
 } else {
   console.log(newZealand);
@@ -50,15 +56,18 @@ kayaking = kayaking.toLowerCase();
 
 if(kayaking.includes('yes') && lovesCoffee.includes('yes') && newZealand.includes('no')) {
   console.log(kayaking);
-  alert('Wow! Correct!! Again!!! It\'s like you know me already....');
+  console.log('Correct');
+  alert('Wow! Correct!! Again!!! It\'s like you know me already ' + userFirstName + '....');
   alert('I once crossed the Puget Sound from Marysville to Beverly Beach on Whidby Island. It took over 8 hours round trip!');
 } else if(kayaking.includes('yes')) {
   console.log(kayaking);
-  alert('Correct!');
+  console.log('Correct');
+  alert('Correct! Nice work ' + userFirstName + '!');
   alert('I once crossed the Puget Sound from Marysville to Beverly Beach on Whidby Island. It took over 8 hours round trip!');
 } else if(kayaking.includes('no')) {
   console.log(kayaking);
-  alert('Incorrect.');
+  console.log('Incorrect.');
+  alert('Sorry. That is not correct ' + userFirstName + '.');
   alert('I once crossed the Puget Sound from Marysville to Beverly Beach on Whidby Island. It took over 8 hours round trip!');
 } else {
   console.log(kayaking);
@@ -71,17 +80,20 @@ attendedCollegeInArizona = attendedCollegeInArizona.toLowerCase();
 
 if(attendedCollegeInArizona.includes('no') && kayaking.includes('yes') && lovesCoffee.includes('yes') && newZealand.includes('no')) {
   console.log(attendedCollegeInArizona);
-  alert('Amazing! Correct again!');
+  console.log('Correct');
+  alert('Killin it ' + userFirstName + '!! Correct again!!');
   alert('Four times in a row!');
   alert('I have not yet done any studying in California. I attended the University of Arizona in Tucson, AZ. Go Wildcats!');
-  alert('Ok! Perfect 4 for 4 so far!');
+  alert('Ok ' + userFirstName + '! A perfect 4 for 4 so far!');
 } else if(attendedCollegeInArizona.includes('no')) {
   console.log(attendedCollegeInArizona);
-  alert('Correct!');
+  console.log('Correct');
+  alert('Correct! Nice work ' + userFirstName + '!');
   alert('I have not yet done any studying in California. I attended the University of Arizona in Tucson, AZ. Go Wildcats!');
 } else if(attendedCollegeInArizona.includes('yes')) {
   console.log(attendedCollegeInArizona);
-  alert('Incorrect.');
+  console.log('Incorrect.');
+  alert('Sorry. That is not correct ' + userFirstName + '.');
   alert('I have not yet done any studying in California. I attended the University of Arizona in Tucson, AZ. Go Wildcats!');
 } else {
   console.log(attendedCollegeInArizona);
@@ -96,19 +108,26 @@ lengthOfEmployment = lengthOfEmployment.toLowerCase();
 
 if(lengthOfEmployment.includes('yes') && attendedCollegeInArizona.includes('no') && kayaking.includes('yes') && lovesCoffee.includes('yes') && newZealand.includes('no')) {
   console.log(lengthOfEmployment);
-  alert('Correct!!! We must be related...');
+  console.log('Correct');
+  alert('Correct!!! Are we related??');
   alert('I have been employed with TJMaxx for 11 years. Full time for 7 years and in management for 5 years.');
-  alert('!PERFECT SCORE! CONGRATULATIONS!');
+  alert('PERFECT SCORE! CONGRATULATIONS!');
+  alert('Hope you enjoyed the game ' + userFirstName + ' and welcome again to my webpage!');
 } else if(lengthOfEmployment.includes('yes')) {
   console.log(lengthOfEmployment);
-  alert('Correct!');
+  console.log('Correct');
+  alert('Correct! Nice work ' + userFirstName + '!');
   alert('I have been employed with TJMaxx for 11 years. Full time for 7 years and in management for 5 years.');
+  alert('Hope you enjoyed the game ' + userFirstName + ' and welcome again to my webpage!');
 } else if(lengthOfEmployment.includes('no')) {
   console.log(lengthOfEmployment);
-  alert('Incorrect.');
+  console.log('Incorrect.');
+  alert('Sorry. That is not correct ' + userFirstName + '.');
   alert('I have been employed with TJMaxx for 11 years. Full time for 7 years and in management for 5 years.');
+  alert('Hope you enjoyed the game ' + userFirstName + ' and welcome again to my webpage!');
 } else {
   console.log(lengthOfEmployment);
   console.log('Invalid Answer');
   alert('Huh? That\'s not a yes or no answer...');
+  alert('Hope you enjoyed the game ' + userFirstName + ' and welcome again to my webpage!');
 }
